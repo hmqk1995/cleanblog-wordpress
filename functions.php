62f9d403b5344d93d2e73e9134df38e71443b157
钱锟 <qiankun@wepiao.com>
2017-06-04 18:27:00 +0800

    move scripts to functionphp

--- functions.php
+++ functions.php
@@ -32,6 +32,16 @@ function cleanblog_scripts() {
     /* font-awesome */
     wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css');
 
+    /* fonts */
+    wp_enqueue_style('googlefont1', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
+    wp_enqueue_style('googlefont2', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
+
+    /* HTML5 IE Support scripts */
+    wp_enqueue_script('html5shiv', '//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js');
+    wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
+    wp_enqueue_script('respond', '//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js');
+    wp_script_add_data( 'respond', 'conditional', 'lt IE 9' );
+
     /* styles */
     wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/clean-blog.min.css');
     wp_enqueue_style('style', get_stylesheet_uri());
